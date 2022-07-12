0e15cb6a011acbd49f02e7228040bcbca6d6793c
Aliceany Gomes <aliceanygomes@hotmail.com>
2022-07-12 15:20:53 -0300

    Adiciona os exemplos do Capitulo 2

---
+++ livraria/src/livraria/EstoqueTeste.java
@@ -0,0 +1,15 @@
+package livraria;
+
+public class EstoqueTeste {
+	public static void main(String[] args) {
+
+		int numero = 4;
+		int outroNumeroIgual = numero;
+		numero = numero + 5;
+
+		System.out.println(numero);
+		System.out.println(outroNumeroIgual);
+
+	}
+
+}
